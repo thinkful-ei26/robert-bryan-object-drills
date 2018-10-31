698a338ad16627a2a1c6dbaa4d5d3c2a2daba7fc
Fantosism <businessspamfolder@gmail.com>
2018-10-31 12:48:43 -0700

    Drill 3-3

---
+++ Day-3-03-03.js
@@ -0,0 +1,10 @@
+// Create an object with a property called meals which is an array of strings: 'breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'.
+// Use console.log to show the name of a hobbit's fourth meal of the day.
+// Don't forget that humans and hobbits count from 1, but computers count from 0.
+'use strict';
+
+const myObj = {
+  meals: ['breakfast', 'second breakfast', 'elevenses', 'lunch', 'afternoon tea', 'dinner', 'supper'],
+};
+
+console.log(myObj.meals[3]);
